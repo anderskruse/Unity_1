464fe2b07e52f640c28881adaacf28c3a1c32d63
Anderskruse <32634165+anderskruse@users.noreply.github.com>
2019-01-30 16:18:06 +0100

    Green, yellow and red complete

---
+++ Assignment 1/Assets/Scripts/Button1Script.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button1Script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Debug.Log("Respect");
+        }
+
+    }
+    public void printText()
+    {
+        Debug.Log("Button 1 Clicked!!!!!!!!");
+    }
+
+    public void playSound()
+    {
+        GetComponent<AudioSource>().Play();
+    }
+
+    public void spawnObject()
+    {
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.position = new Vector3(0, 1.5f, 0);
+        sphere.AddComponent<Rigidbody>();
+    }
+
+    public void doNotPress()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            sphere.transform.position = new Vector3(0, 1.5f, 0);
+            sphere.AddComponent<Rigidbody>();
+
+        }
+        
+    }
+
+}
---
+++ Assignment 1/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public GameObject sphere;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(sphere.transform.position.x, transform.position.y, sphere.transform.position.z);
+        transform.LookAt(sphere.transform.position);
+    }
+}
