d29ee1a2f7fbf69aad66d6d35edc428d884769d7
Anderskruse <32634165+anderskruse@users.noreply.github.com>
2019-01-31 08:34:34 +0100

    The 'do not press' spheres are now rendered with random colors

--- Assignment 1/Assets/Scripts/Button1Script.cs
+++ Assignment 1/Assets/Scripts/Button1Script.cs
@@ -13,10 +13,13 @@ public class Button1Script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         if (Input.GetKeyDown(KeyCode.F))
         {
             Debug.Log("Respect");
+
         }
+
 
     }
     public void printText()
@@ -38,14 +41,16 @@ public class Button1Script : MonoBehaviour
 
     public void doNotPress()
     {
-        for (int i = 0; i < 100; i++)
+        for (int i = 0; i < 300; i++)
         {
             GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
             sphere.transform.position = new Vector3(0, 1.5f, 0);
             sphere.AddComponent<Rigidbody>();
+            sphere.GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+
 
         }
-        
+
     }
 
 }
